4109e3af222960fe9159f72f9751b7f16e2791fa
Antonio Rodríguez <antonio@iessanvicente.com>
2015-11-07 18:10:19 +0100

    Versión final

---
+++ ComboBoxBD/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(13, 41)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Object"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "DataSource"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(13, 102)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 148)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "ComboBox BD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+End Class
---
+++ ComboBoxBD/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System
+Imports System.Data
+Imports Npgsql
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim bd As New BaseDeDatos
+
+        'Rellenamos el combobox con Objetos de tipo Juego
+        For Each fila As DataRow In bd.ListarPaises().Rows
+            ComboBox1.Items.Add(New Juego(fila(0), fila(1)))
+        Next
+
+        'ASignamos un DataSet con lo juegos al DataSource y mediante ValueMember y DisplayMember obtenemos el valor seleccionado
+        ComboBox2.ValueMember = "id_idonline"
+        ComboBox2.DisplayMember = "nombre_idonline"
+        ComboBox2.DataSource = bd.ListarPaises()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        MsgBox("He seleccionado el Juego con ID:" & CType(ComboBox1.SelectedItem, Juego).id_idonline)
+
+    End Sub
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        MsgBox("He seleccionado el Juego con ID:" & ComboBox2.SelectedValue)
+    End Sub
+End Class
+
+''' <summary>
+''' Mini Clase Juego
+''' </summary>
+Public Class Juego
+    Public Property id_idonline As UInteger
+    Public Property nombre_idonline As String
+
+    Public Sub New(ByVal pId As UInteger, ByVal pNom As String)
+        Me.id_idonline = pId
+        Me.nombre_idonline = pNom
+    End Sub
+    Public Overrides Function ToString() As String
+        Return nombre_idonline
+    End Function
+End Class
+
+Public Class BaseDeDatos
+    Private ConexionConBD As NpgsqlConnection
+    Private Orden As NpgsqlCommand
+    Private Lector As NpgsqlDataReader
+
+    Public Function ListarPaises() As DataTable
+        Dim Juegos As DataTable
+        Juegos = New DataTable
+        Dim strConexión As String = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
+                                                  "localhost", "5432", "esports", "antonio", "esports")
+        ConexionConBD = New NpgsqlConnection(strConexión)
+        ConexionConBD.Open()
+
+        'Crear una consulta
+        Dim Consulta As String = "SELECT id_idonline, nombre_idonline FROM IDONLINE ORDER BY 2"
+        Orden = New NpgsqlCommand(Consulta, ConexionConBD)
+        Lector = Orden.ExecuteReader()
+        Juegos.Load(Lector)
+        CerrarConexion()
+        Return Juegos
+    End Function
+
+
+    Public Sub CerrarConexion()
+        ' Cerrar la conexión cuando ya no sea necesaria
+        If (Not Lector Is Nothing) Then
+            Lector.Close()
+        End If
+        If (Not ConexionConBD Is Nothing) Then
+            ConexionConBD.Close()
+        End If
+    End Sub
+
+End Class
